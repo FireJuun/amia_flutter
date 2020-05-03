8c9f6a8ddb7c6f2b5a2f45037398c3f217a73e00
John Manning <john.d.manning@gmail.com>
2020-05-03 16:50:40 -0400

    fix: changed symptom enums to all be unique

--- lib/services/symptom.service.dart
+++ lib/services/symptom.service.dart
@@ -14,57 +14,57 @@ class SymptomService extends GetController {
     ),
     Symptom(
       name: 'Short of Breath',
-      id: symptomId.cough,
+      id: symptomId.sob,
       image: Image.asset('assets/icons/SOB.png'),
     ),
     Symptom(
       name: 'Headache',
-      id: symptomId.cough,
+      id: symptomId.headache,
       image: Image.asset('assets/icons/headache.png'),
     ),
     Symptom(
       name: 'Feeling Ill',
-      id: symptomId.cough,
+      id: symptomId.feelingIll,
       image: Image.asset('assets/icons/feeling_ill.png'),
     ),
     Symptom(
       name: 'Body Aches',
-      id: symptomId.cough,
+      id: symptomId.bodyAches,
       image: Image.asset('assets/icons/body_aches.png'),
     ),
     Symptom(
       name: 'Weird/No Taste',
-      id: symptomId.cough,
+      id: symptomId.taste,
       image: Image.asset('assets/icons/taste.png'),
     ),
     Symptom(
       name: 'Weird/No Smell',
-      id: symptomId.cough,
+      id: symptomId.smell,
       image: Image.asset('assets/icons/smell.png'),
     ),
     Symptom(
       name: 'Vomiting',
-      id: symptomId.cough,
+      id: symptomId.vomiting,
       image: Image.asset('assets/icons/vomiting.png'),
     ),
     Symptom(
       name: 'Diarrhea',
-      id: symptomId.cough,
+      id: symptomId.diarrhea,
       image: Image.asset('assets/icons/diarrhea.png'),
     ),
     Symptom(
       name: 'Sneezing',
-      id: symptomId.cough,
+      id: symptomId.sneezing,
       image: Image.asset('assets/icons/sneezing.png'),
     ),
     Symptom(
       name: 'Runny Nose',
-      id: symptomId.cough,
+      id: symptomId.runnyNose,
       image: Image.asset('assets/icons/runny_nose.png'),
     ),
     Symptom(
       name: 'Sore Throat',
-      id: symptomId.cough,
+      id: symptomId.soreThroat,
       image: Image.asset('assets/icons/sore_throat.png'),
     ),
   ];
