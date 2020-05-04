1e794e5e16947a2de8d65300bf03d66ba705dbf3
John Manning <john.d.manning@gmail.com>
2020-05-03 22:44:57 -0400

    barebones checkin screen

--- lib/screens/checkin.screen.dart
+++ lib/screens/checkin.screen.dart
@@ -1,16 +1,9 @@
 import 'package:amia_flutter/screens/screens.dart';
 import 'package:amia_flutter/shared/shared.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_icons/flutter_icons.dart';
 import 'package:get/get.dart';
 
-class CheckinScreen extends StatefulWidget {
-  @override
-  _CheckinScreenState createState() => _CheckinScreenState();
-}
-
-class _CheckinScreenState extends State<CheckinScreen> {
-  @override
+class CheckinScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: SharedAppBar(
@@ -27,15 +20,12 @@ class _CheckinScreenState extends State<CheckinScreen> {
       //Containers are kind of like div in html
       //but can only contain one child widget
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
+        child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              SizedBox(height: 48),
               _buildHeader(),
-              SizedBox(height: 24),
               _buildSymptomCheckboxes(),
-              SizedBox(height: 24),
               _buildSubmitButton(),
             ],
           ),
@@ -45,35 +35,11 @@ class _CheckinScreenState extends State<CheckinScreen> {
   }
 
   Widget _buildHeader() {
-    TextTheme _textTheme = Get.theme.textTheme;
-    return Column(
-      children: <Widget>[
-        Text(
-          'Please select your symptoms',
-          textAlign: TextAlign.center,
-          style: _textTheme.display1,
-        ),
-        SizedBox(height: 8),
-        Text(
-          'Yesterday, you had cough, short of breath, and body aches.',
-          textAlign: TextAlign.center,
-          style: _textTheme.headline,
-        )
-      ],
-    );
+    return Text('Header goes here', style: Get.theme.textTheme.display1);
   }
 
   Widget _buildSymptomCheckboxes() {
-    return Expanded(
-      child: ListView(
-        children: <Widget>[
-          _buildCheckbox(name: 'Cough', icon: FlutterIcons.face_mco),
-          _buildCheckbox(name: 'Cough', icon: FlutterIcons.face_mco),
-          _buildCheckbox(name: 'Cough', icon: FlutterIcons.face_mco),
-          _buildCheckbox(name: 'Cough', icon: FlutterIcons.face_mco),
-        ],
-      ),
-    );
+    return Text('Symptoms go here');
   }
 
   Widget _buildSubmitButton() {
@@ -83,25 +49,6 @@ class _CheckinScreenState extends State<CheckinScreen> {
         Get.snackbar('Check-in complete', 'Be sure to check-in tomorrow as well!');
         Get.back();
       },
-    );
-  }
-
-  // todo: extract into shared widget
-  Widget _buildCheckbox({@required String name, @required IconData icon}) {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Icon(icon),
-        Expanded(
-          child: Text(name, textAlign: TextAlign.center, style: Get.theme.textTheme.subtitle),
-        ),
-        Checkbox(
-          value: false,
-          activeColor: Get.theme.primaryColor,
-          onChanged: (bool value) {},
-        ),
-      ],
     );
   }
 }
