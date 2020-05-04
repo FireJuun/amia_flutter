1bd444e4131d5082946b7c356845d38104b434ad
John Manning <john.d.manning@gmail.com>
2020-05-03 22:00:09 -0400

    removed enums enums are useful for finding class instances w/o searching for 'string'

--- lib/services/symptom.model.dart
+++ lib/services/symptom.model.dart
@@ -4,22 +4,6 @@ class Symptom {
   bool isChecked;
   String name;
   IconData icon;
-  symptomId id;
 
-  Symptom({this.isChecked = false, @required this.icon, @required this.name, @required this.id});
-}
-
-enum symptomId {
-  bodyAches,
-  cough,
-  diarrhea,
-  feelingIll,
-  headache,
-  runnyNose,
-  smell,
-  sneezing,
-  sob,
-  soreThroat,
-  taste,
-  vomiting
+  Symptom({this.isChecked = false, @required this.icon, @required this.name});
 }
