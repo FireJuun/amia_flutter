7fcb83a986e3e5b59412441eeeee497c38895be3
John Manning <john.d.manning@gmail.com>
2020-05-04 10:27:19 -0400

    trimmed all content from dashboard/checkin screens

--- lib/screens/checkin.screen.dart
+++ lib/screens/checkin.screen.dart
@@ -19,36 +19,7 @@ class CheckinScreen extends StatelessWidget {
       ),
       //Containers are kind of like div in html
       //but can only contain one child widget
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              _buildHeader(),
-              _buildSymptomCheckboxes(),
-              _buildSubmitButton(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildHeader() {
-    return Text('Header goes here', style: Get.theme.textTheme.display1);
-  }
-
-  Widget _buildSymptomCheckboxes() {
-    return Text('Symptoms go here');
-  }
-
-  Widget _buildSubmitButton() {
-    return SharedActionButton(
-      title: 'Submit',
-      onPressed: () {
-        Get.snackbar('Check-in complete', 'Be sure to check-in tomorrow as well!');
-        Get.back();
-      },
+      body: Center(child: Text('Check-in')),
     );
   }
 }
--- lib/screens/dashboard.screen.dart
+++ lib/screens/dashboard.screen.dart
@@ -6,42 +6,20 @@ class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _buildAppBar(),
-      body: _buildBody(),
-    );
-  }
-
-  Widget _buildAppBar() {
-    return SharedAppBar(
-      title: 'Dashboard',
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(
-            Icons.business,
-            // Default is 24, otherwise IconButtons need to be set manually
-            size: Get.theme.iconTheme.size,
-          ),
-          onPressed: () => Get.back(),
-        )
-      ],
-    );
-  }
-
-  Widget _buildBody() {
-    return ListView(
-      children: <Widget>[
-        Text('Good morning, AMIA', style: Get.theme.textTheme.display2, textAlign: TextAlign.center),
-        Text('Check-in'),
-        Text('History'),
-        Text('Contact tracing'),
-        Text('Info'),
-        ListTile(
-          leading: Text('leading'),
-          title: Text('ListTile'),
-          subtitle: Text('Subtitle'),
-          trailing: Text('trailing'),
-        ),
-      ],
+      appBar: SharedAppBar(
+        title: 'Dashboard',
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.business,
+              // Default is 24, otherwise IconButtons need to be set manually
+              size: Get.theme.iconTheme.size,
+            ),
+            onPressed: () => Get.back(),
+          )
+        ],
+      ),
+      body: Center(child: Text('Dashboard')),
     );
   }
 }
